1afe69133017c38110a134dffb7ee7345d523db8
2290142073@qq.com <2290142073@qq.com>
2019-12-23 21:47:59 +0800

    change the queue for better performance

--- byteps/common/scheduled_queue.h
+++ byteps/common/scheduled_queue.h
@@ -58,7 +58,7 @@ class BytePSScheduledQueue {
   int _dooropen = 11;  
   int _grad_checkpoint[13] = {0,10,23,36,51,63,78,91,104,118,131,144,157};
   // int _middle[12] = {5,22,35,50,62,68,84,95,108,130,135,147};
-  int _middle[12] = {5,18,30,45,62,68,84,95,108,125,135,147};
+  int _middle[12] = {5,16,28,40,55,65,80,93,106,120,135,147};
   // int _tensor_part[160] = {0};
   // int _tensor_numpull = 0;
   // int _dooropenpull = 11;
