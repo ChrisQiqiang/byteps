c62a98db605d8794a54e3a42c93bff0016c87d86
2290142073@qq.com <2290142073@qq.com>
2020-01-02 16:09:51 +0800

    env

--- byteps/common/scheduled_queue.cc
+++ byteps/common/scheduled_queue.cc
@@ -39,7 +39,7 @@ BytePSScheduledQueue::BytePSScheduledQueue(QueueType type) {
     if(!strcmp(getenv("MODEL"), "vgg19"))
     {
       int tmp1[13] = {-1, 1, 13, 27, 37, 0, 77, 90, 103, 117, 130, 143, 156};
-      int tmp2[13] = {8.3, 49.05, 71.35, 0, 0, 53, 44, 64, 90, 74, 58, 15, 0}; // backward execution time
+      int tmp2[13] = { 71.35, 49.05, 8.3, 0, 0, 53, 44, 64, 90, 74, 58, 15, 0}; // backward execution time
       int _init_pointer = 4;
       for(int i = 0; i <= _init_pointer; i++){
         _grad_checkpoint[i] = tmp1[i];
